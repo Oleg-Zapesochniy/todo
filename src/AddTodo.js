745b403f63aa3f86be9839e1a84d15a51aad7a19
Oleg_Zapesochniy <1konstriktor@gmail.com>
2022-06-19 22:03:29 +0300

    input button

---
+++ src/AddTodo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, StyleSheet, TextInput, Button} from 'react-native';
+
+export const AddTodo = ({onSubmit}) => {
+
+    const pressHandler = () => {
+      onSubmit('testTodo')
+    }
+    return (
+        <View style={styles.block}>
+          <TextInput style={styles.input}/>
+          <Button title="Добавить" onPress={pressHandler} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      allignItems: 'center'
+    },
+    input :{  
+      width: '70%',
+      borderBottomColor: '#191',
+      borderBottomWidth: 2 , 
+      borderStyle: 'solid',
+      padding: 10,
+    },
+    button: {
+      color: 'red'
+    }
+})
---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export const Navbar = (props) => {
+    return (
+        <View style={styles.navbar}>
+            <Text style = {styles.text}> {props.title} </Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    navbar: {
+      height:70,
+      alignItems:'center',
+      justifyContent: 'flex-end',
+      backgroundColor:"#4f4",
+      paddingBottom: "up",
+    },
+    text:{
+      color:'white',
+      fontSize:20,
+    }
+})
