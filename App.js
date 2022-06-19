bbeab569f9b7c129f30766646bb06d4f7ed3ee5b
Oleg_Zapesochniy <1konstriktor@gmail.com>
2022-06-19 21:39:36 +0300

    input button

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+  //   const newTodo = {
+  //     id: Date.now().toString(),
+  //     title: title
+  //   }
+
+    // setTodos(todos.concat([newTodo]))
+    
+    // setTodos((prevTodos)=> {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev =>[
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title = "TODO app"/>
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map(todo => {
+            return (
+              <Text key={todo.id}>{todo.title}</Text>
+            )
+          })}
+        </View>
+        {/* просто ссылка на ф-цюи, ее не вызываем */}
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+
+
+    // // flex: 1,
+    // height:300,
+    // backgroundColor: '#4ff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
